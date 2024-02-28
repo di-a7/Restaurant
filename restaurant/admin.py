72fae12da7fed7e4795fa03bd8777f51efb3d24a
Diya <shresthadiya59@gmail.com>
2024-02-28 16:41:56 +0545

    models admin

---
+++ restaurant/admin.py
@@ -0,0 +1,91 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=['name','category','description','display_price'] 
+    search_fields=('name','category')
+    list_filter=('category','price',)
+    list_per_page=15
+    
+    def display_price(self,obj):
+        return f"Rs. {obj.price}"
+    display_price.short_description = 'Price'
+
+class TableInline(admin.TabularInline):
+    model = Table
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display= ['id','user','order_type','status','table','display_order_items','total_amount']
+    list_editable= ('status',)
+    search_fields=('user','order_type','status',)
+    list_filter=('order_type','status','table','user')
+    list_per_page=15
+    
+    inlines = (OrderItemInline,)
+    
+    def display_order_items(self, obj):
+        order_items = ", ".join([f"{item.quantity}x {item.menu.name} ({item.special_requests}.)" for item in obj.items.all()])
+        return order_items if order_items else "-"
+    display_order_items.short_description = "Order Items"
+    
+    def total_amount(self, obj):
+        total = sum(item.menu.price * item.quantity for item in obj.items.all())
+        return total
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display= ['table_number','capacity','status']
+    list_editable= ('status',)
+    search_fields=('capacity','status')
+    list_filter=('table_number','status','capacity')
+    
+@admin.register(Kitchen)
+class KitchenAdmin(admin.ModelAdmin):
+    list_display=['order','display_special_requests','preparation_status']
+    list_editable= ('preparation_status',)
+    search_fields=('preparation_status',)
+    list_filter=('preparation_status','order',)
+    
+    def display_special_requests(self, obj):
+        order_items = " ".join([f"{item.quantity}x {item.menu.name} ({item.special_requests}.)" for item in obj.order.items.all()])
+        return order_items if order_items else "-"
+    display_special_requests.short_description = "Order Item"
+    
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display=['order','display_order_items','total_amount','generated_by','generated_at']
+    search_fields=('generated_by__username',)
+    list_filter=('order','generated_by','order__table__table_number',)
+    list_per_page=15
+    
+    def display_order_items(self, obj):
+        order_items = ", ".join([f"{item.quantity}x {item.menu.name} ({item.special_requests}.)" for item in obj.order.items.all()])
+        return order_items if order_items else "-"
+    display_order_items.short_description = "Order Items"
+    
+    def total_amount(self, obj):
+        total = sum(item.menu.price * item.quantity for item in obj.order.items.all())
+        return total
+    total_amount.short_description = "Total Amount"
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display=['name']
+    search_fields=('name',)
+    list_per_page=15
+    
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display=['ingredient','quantity','unit_of_measurement','used_quantity','expiration_date']
+    list_editable= ('used_quantity',)
+    search_fields= ('ingredient__name','quantity','expiration_date')
+    autocomplete_fields = ('ingredient',)
+    list_filter=('ingredient','used_quantity','expiration_date')
+    list_per_page=15
+    
---
+++ restaurant/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Menu(models.Model):
+    CATEGORY_CHOICES = [
+        ('appetizer', 'Appetizer'),
+        ('breakfast', 'Breakfast'),
+        ('salads', 'Salads'),
+        ('main', 'Main'),
+        ('dessert', 'Dessert'),
+        ('drink', 'Drink'),
+        ('side_dish', 'Side Dish'),
+    ]
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+class Table(models.Model):
+    STATUS_CHOICES=[
+        ('available','Available'),
+        ('occupied','Occupied'),
+        ('reserved','Reserved'),
+    ]
+    table_number = models.IntegerField(unique=True)
+    capacity=models.IntegerField()
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default='available')
+
+    def __str__(self):
+        return f"Table {self.table_number}"
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('received', 'Received'),
+        ('preparation', 'In Preparation'),
+        ('ready', 'Ready for Delivery/Pickup'),
+        ('delivered', 'Delivered'),
+    ]
+    ORDER_TYPE_CHOICES=[
+        ('dine-in','Dine-In'),
+        ('takeout','Takeout'),
+        ('delivery','Delivery'),
+    ]
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,null=True, blank=True)
+    order_type = models.CharField(max_length=20, choices=ORDER_TYPE_CHOICES)
+    table=models.ForeignKey(Table,on_delete=models.CASCADE, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='received')
+    # created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} ({self.get_status_display()})"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    special_requests = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.menu.name} - {self.order}"
+    
+class Kitchen(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('prepared', 'Prepared'),
+        ('served', 'Served'),
+    ]
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='kitchen')
+    preparation_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    
+    def __str__(self):
+        return f"Kitchen Order #{self.id} ({self.preparation_status})"
+    
+class Bill(models.Model):
+    order = models.OneToOneField(Order, on_delete = models.CASCADE)
+    # total_amount = models.DecimalField(max_digits=10,decimal_places=2)
+    generated_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    generated_at = models.DateTimeField(auto_now_add = True)
+    def __str__(self):
+        return f"Bill for Order #{self.order.id}"
+    
+    # def save(self, *args, **kwargs):
+    #     total_amount = sum(item.menu.price * item.quantity for item in self.order.items.all())
+    #     self.total_amount = total_amount
+    #     super().save(*args, **kwargs)
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class InventoryItem(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_of_measurement = models.CharField(max_length=50)
+    used_quantity = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True,blank=True)
+    expiration_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.ingredient.name} - {self.used_quantity}/{self.quantity}{self.unit_of_measurement} {self.ingredient}"
